564d6660b1b0454139bb99dff432b13676dd9a88
Patrick <papybp@gmail.com>
2022-02-22 21:09:01 -0600

    w1d2

--- w1d2/compoundInterest.js
+++ w1d2/compoundInterest.js
@@ -8,8 +8,19 @@
 * @returns {int} The compound Interest.
 */
 function compoundInterest (initialAmount, annualInterestRate, numYearCompound){
-
+    let balance = initialAmount;
+    let monthlyInterest = annualInterestRate / 100 / 12;
+    let numOfMonths = numYearCompound * 12;
     
+    for (let i = 1; i <= numOfMonths; i++) {
+    
+    balance += (balance * monthlyInterest);
+    
+    }
+    
+    return balance.toFixed(2);
+      
+  
 
 }
 
