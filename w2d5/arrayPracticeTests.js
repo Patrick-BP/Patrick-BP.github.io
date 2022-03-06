958be42b327f27f936246c4589e8bda55dc12d78
Patrick <papybp@gmail.com>
2022-03-06 15:47:25 -0600

    w2d5

--- w2d5/arrayPracticeTests.js
+++ w2d5/arrayPracticeTests.js
@@ -2,16 +2,16 @@
 /* global assert doubleNums  doubleAges filterEven filterOver10 findEvenNum findEvenAge */
 /* comment out the node specific code when going to the browser
 */
-const assert = require("assert");  //always need this with node
-const myExports = require("./arrayPractice.js");  //with node need the name of your file with your functions here
-const doubleNums = myExports.doubleNums;  //do this for all of the functions used in the Mocha tests
-const doubleAges = myExports.doubleAges;
-const filterEven = myExports.filterEven;
-const filterOver10 = myExports.filterOver10;
-const findEvenNum = myExports.findEvenNum;
-const findEvenAge = myExports.findEvenAge;  
-const includesEvenAge = myExports.includesEvenAge;
-const includesEvenNum = myExports.includesEvenNum;
+// const assert = require("assert");  //always need this with node
+// const myExports = require("./arrayPractice.js");  //with node need the name of your file with your functions here
+// const doubleNums = myExports.doubleNums;  //do this for all of the functions used in the Mocha tests
+// const doubleAges = myExports.doubleAges;
+// const filterEven = myExports.filterEven;
+// const filterOver10 = myExports.filterOver10;
+// const findEvenNum = myExports.findEvenNum;
+// const findEvenAge = myExports.findEvenAge;  
+// const includesEvenAge = myExports.includesEvenAge;
+// const includesEvenNum = myExports.includesEvenNum;
 
 let numArray;
 let peopleArray;
