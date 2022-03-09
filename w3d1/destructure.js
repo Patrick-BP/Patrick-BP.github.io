fac9530eb312d5085aecf8fec0b68e2f1cd145e6
Patrick <papybp@gmail.com>
2022-03-08 21:11:40 -0600

    w3d2

--- w3d1/destructure.js
+++ w3d1/destructure.js
@@ -9,24 +9,66 @@ let salaries = {
 };
 
 function topSalary(salaries) {
- let max =0;
- let name = null;
- 
-    if(salaries === {}){
-      return null;
-    }else{
-      for (const [key, value] of Object.entries(salaries)) {
-        if(max < value){
-          max = value;
-          name = key;
-        } 
-    }  
+  let max = 0;
+  let name = null;
+
+  if (Object.entries(salaries).length == 0) {
+    return null;
+  } else {
+    for (const [key, value] of Object.entries(salaries)) {
+      if (max < value) {
+        max = value;
+        name = key;
+      }
+    }
   }
   return name;
-} 
- // json this can be used to clone objects
-function clone(obj){
+}
+// json this can be used to clone objects
+function clone(obj) {
   let json = JSON.stringify(obj);
   console.log(json);
   return JSON.parse(json);
+}
+
+// setInterval
+function printNumbers(from, to){
+  let timerId = setInterval(()=>{
+ console.log(from)
+  if(from === to){
+    clearInterval(timerId);
+  }
+    from++;
+}, 1000);
+
+}
+console.log(printNumbers(5,10))
+
+
+//setTimeout 
+
+function printNumbers2(from, to) {
+  let num = from;
+  setTimeout(function fun1() {
+    console.log(num);
+    if (num < to) {
+      setTimeout(fun1, 1000);
+    }
+    num++;
+  }, 1000);
+
+}
+
+console.log(printNumbers2(5, 10));
+
+
+
+
+//What will setTimeout show?
+
+let i = 0;
+
+setTimeout(() => alert(i), 100); // 100000000
+for(let j = 0; j < 100000000; j++) {
+  i++;
 }
