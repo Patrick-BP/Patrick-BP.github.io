9c029a825766bcbab79c3c50a0fa10c7458bec4f
Patrick <papybp@gmail.com>
2022-04-12 23:51:41 -0500

    ghjkghjk

--- practice/CS315/recursion2.js
+++ practice/CS315/recursion2.js
@@ -4,76 +4,99 @@
 let simpleObject = {a:1, b:2,c:{a:[1,2]}};
 
 // ======================== Question 1 ========================
- function countKeys(obj){
-   let count= 1;
-    for (const elem of Object.keys(obj)) {
-        if(typeof obj[elem] ==="object"){
-            if((Array.isArray(obj[elem]))){
-                continue;
-            }else{
-             count +=  countKeys(obj[elem]);
-            }  
-        } 
-         count++; 
-    }
-   return count;
- }
-console.log(countKeys(simpleObject));
+//  function countKeys(obj){
+//    let count= 1;
+//     for (const elem of Object.keys(obj)) {
+//         if(typeof obj[elem] ==="object"){
+//             if((Array.isArray(obj[elem]))){
+//                 continue;
+//             }else{
+//              count +=  countKeys(obj[elem]);
+//             }  
+//         } 
+//          count++; 
+//     }
+//    return count;
+//  }
+// console.log(countKeys(simpleObject));
 
 //==================== Question 2 =========================
 
-function listLeaves(obj){
-let arr =[];
-for (const elem of Object.keys(obj)) {
-            if(typeof obj[elem] ==="object" && !(Array.isArray(obj[elem]))){
+// function listLeaves(obj){
+// let arr =[];
+// for (const elem of Object.keys(obj)) {
+//             if(typeof obj[elem] ==="object" && !(Array.isArray(obj[elem]))){
             
-               arr =  arr.concat(listLeaves(obj[elem]));
+//                arr =  arr.concat(listLeaves(obj[elem]));
                  
-            } else{
-                arr.push({[elem]: obj[elem]}); 
-            }  
-        }
-        return arr;
-}
-console.log(listLeaves(simpleObject));
+//             } else{
+//                 arr.push({[elem]: obj[elem]}); 
+//             }  
+//         }
+//         return arr;
+// }
+// console.log(listLeaves(simpleObject));
 
 
 // ======================== Question 3 ===================================
 
-function findAllKeysWithGivenValue(obj, target){
-    let arr = [];
-    for (const elem of Object.keys(obj)) {
-        if(typeof obj[elem] ==="object" && !(Array.isArray(obj[elem]))){
+// function findAllKeysWithGivenValue(obj, target){
+//     let arr = [];
+//     for (const elem of Object.keys(obj)) {
+//         if(typeof obj[elem] ==="object" && !(Array.isArray(obj[elem]))){
                     
-            arr =  arr.concat(findAllKeysWithGivenValue(obj[elem], 1));
+//             arr =  arr.concat(findAllKeysWithGivenValue(obj[elem], 1));
                          
-            } else{
-                if(obj[elem] === target){
-                     arr.push(elem);
-                }else if(Array.isArray(obj[elem])){
-                    for(let el of obj[elem]){
+//             } else{
+//                 if(obj[elem] === target){
+//                      arr.push(elem);
+//                 }else if(Array.isArray(obj[elem])){
+//                     for(let el of obj[elem]){
                         
-                        if(el == target){
-                            arr.push(elem);
-                        }
-                    }
-                }
+//                         if(el == target){
+//                             arr.push(elem);
+//                         }
+//                     }
+//                 }
             
-            }  
-        }
-        return arr;
-}
+//             }  
+//         }
+//         return arr;
+// }
 
-console.log(findAllKeysWithGivenValue(simpleObject, 1))
+// console.log(findAllKeysWithGivenValue(simpleObject, 1))
 
 // ======================== Question 4 ===================================
+let json = {
+    "glossary": {
+    "title": "example glossary",
+    "GlossSeeAlso": {
+    "title": "S",
+    "GlossList": {
+    "GlossEntry": {
+    "ID": "SGML",
+    "SortAs": "SGML",
+    "GlossSeeAlso": "Standard Generalized Markup Language",
+    "Acronym": "SGML",
+    "Abbrev": "ISO 8879:1986",
+    "GlossDef": {
+    "para": "A meta-markup language, used to create markup languages such as DocBook.",
+    "GlossSeeAlso": ["GML", "XML","S"]
+    },
+    "GlossSee": "markup"
+    }
+    }
+    }
+    }
+    }
+
 
 function findValuesOfGivenKey(obj, target){
     let arr = [];
     for (const elem of Object.keys(obj)) {
         if(typeof obj[elem] ==="object" && !(Array.isArray(obj[elem]))){
                     
-            arr =  arr.concat(findValuesOfGivenKey(obj[elem], "a"));
+            arr =  arr.concat(findValuesOfGivenKey(obj[elem], "GlossSeeAlso"));
                          
             } else{
                 if(elem === target){
@@ -89,4 +112,7 @@ function findValuesOfGivenKey(obj, target){
         }
         return arr;
 }
-console.log(findValuesOfGivenKey(simpleObject, "a"))
+console.log(findValuesOfGivenKey(json, "GlossSeeAlso"))
+
+
+
