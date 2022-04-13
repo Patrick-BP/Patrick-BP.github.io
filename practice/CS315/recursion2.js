74cb58bd851414a559e5274ff53b67f09c86774f
Patrick <papybp@gmail.com>
2022-04-12 23:39:33 -0500

    bm,bn,bnm,b

--- practice/CS315/recursion2.js
+++ practice/CS315/recursion2.js
@@ -3,6 +3,7 @@
 
 let simpleObject = {a:1, b:2,c:{a:[1,2]}};
 
+// ======================== Question 1 ========================
 //  function countKeys(obj){
 //    let count= 1;
 //     for (const elem of Object.keys(obj)) {
@@ -18,6 +19,8 @@ let simpleObject = {a:1, b:2,c:{a:[1,2]}};
 //    return count;
 //  }
 // console.log(countKeys(simpleObject));
+
+//==================== Question 2 =========================
 
 // function listLeaves(obj){
 // let arr =[];
@@ -35,6 +38,55 @@ let simpleObject = {a:1, b:2,c:{a:[1,2]}};
 // console.log(listLeaves(simpleObject));
 
 
-function findAllKeysWithGivenValue(simpleObject,1){
+// ======================== Question 3 ===================================
 
+// function findAllKeysWithGivenValue(obj, target){
+//     let arr = [];
+//     for (const elem of Object.keys(obj)) {
+//         if(typeof obj[elem] ==="object" && !(Array.isArray(obj[elem]))){
+                    
+//             arr =  arr.concat(findAllKeysWithGivenValue(obj[elem], 1));
+                         
+//             } else{
+//                 if(obj[elem] === target){
+//                      arr.push(elem);
+//                 }else if(Array.isArray(obj[elem])){
+//                     for(let el of obj[elem]){
+                        
+//                         if(el == target){
+//                             arr.push(elem);
+//                         }
+//                     }
+//                 }
+            
+//             }  
+//         }
+//         return arr;
+// }
+
+//console.log(findAllKeysWithGivenValue(simpleObject, 1))
+
+
+
+function findValuesOfGivenKey(obj, target){
+    let arr = [];
+    for (const elem of Object.keys(obj)) {
+        if(typeof obj[elem] ==="object" && !(Array.isArray(obj[elem]))){
+                    
+            arr =  arr.concat(findValuesOfGivenKey(obj[elem], "a"));
+                         
+            } else{
+                if(elem === target){
+                     arr.push(obj[elem]);
+                }else if(Array.isArray(obj[elem])){
+                        if(elem == target){
+                            arr.push(obj[elem]);
+                        }
+                    
+                }
+            
+            }  
+        }
+        return arr;
 }
+console.log(findValuesOfGivenKey(simpleObject, "a"))
