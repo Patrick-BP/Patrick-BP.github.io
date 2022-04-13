fad381282ef54c93fcf7b18ff6dd66805861337e
Patrick <papybp@gmail.com>
2022-04-12 23:40:41 -0500

    bjkhjk

--- practice/CS315/recursion2.js
+++ practice/CS315/recursion2.js
@@ -4,69 +4,69 @@
 let simpleObject = {a:1, b:2,c:{a:[1,2]}};
 
 // ======================== Question 1 ========================
-//  function countKeys(obj){
-//    let count= 1;
-//     for (const elem of Object.keys(obj)) {
-//         if(typeof obj[elem] ==="object"){
-//             if((Array.isArray(obj[elem]))){
-//                 continue;
-//             }else{
-//              count +=  countKeys(obj[elem]);
-//             }  
-//         } 
-//          count++; 
-//     }
-//    return count;
-//  }
-// console.log(countKeys(simpleObject));
+ function countKeys(obj){
+   let count= 1;
+    for (const elem of Object.keys(obj)) {
+        if(typeof obj[elem] ==="object"){
+            if((Array.isArray(obj[elem]))){
+                continue;
+            }else{
+             count +=  countKeys(obj[elem]);
+            }  
+        } 
+         count++; 
+    }
+   return count;
+ }
+console.log(countKeys(simpleObject));
 
 //==================== Question 2 =========================
 
-// function listLeaves(obj){
-// let arr =[];
-// for (const elem of Object.keys(obj)) {
-//             if(typeof obj[elem] ==="object" && !(Array.isArray(obj[elem]))){
+function listLeaves(obj){
+let arr =[];
+for (const elem of Object.keys(obj)) {
+            if(typeof obj[elem] ==="object" && !(Array.isArray(obj[elem]))){
             
-//                arr =  arr.concat(listLeaves(obj[elem]));
+               arr =  arr.concat(listLeaves(obj[elem]));
                  
-//             } else{
-//                 arr.push({[elem]: obj[elem]}); 
-//             }  
-//         }
-//         return arr;
-// }
-// console.log(listLeaves(simpleObject));
+            } else{
+                arr.push({[elem]: obj[elem]}); 
+            }  
+        }
+        return arr;
+}
+console.log(listLeaves(simpleObject));
 
 
 // ======================== Question 3 ===================================
 
-// function findAllKeysWithGivenValue(obj, target){
-//     let arr = [];
-//     for (const elem of Object.keys(obj)) {
-//         if(typeof obj[elem] ==="object" && !(Array.isArray(obj[elem]))){
+function findAllKeysWithGivenValue(obj, target){
+    let arr = [];
+    for (const elem of Object.keys(obj)) {
+        if(typeof obj[elem] ==="object" && !(Array.isArray(obj[elem]))){
                     
-//             arr =  arr.concat(findAllKeysWithGivenValue(obj[elem], 1));
+            arr =  arr.concat(findAllKeysWithGivenValue(obj[elem], 1));
                          
-//             } else{
-//                 if(obj[elem] === target){
-//                      arr.push(elem);
-//                 }else if(Array.isArray(obj[elem])){
-//                     for(let el of obj[elem]){
+            } else{
+                if(obj[elem] === target){
+                     arr.push(elem);
+                }else if(Array.isArray(obj[elem])){
+                    for(let el of obj[elem]){
                         
-//                         if(el == target){
-//                             arr.push(elem);
-//                         }
-//                     }
-//                 }
+                        if(el == target){
+                            arr.push(elem);
+                        }
+                    }
+                }
             
-//             }  
-//         }
-//         return arr;
-// }
+            }  
+        }
+        return arr;
+}
 
-//console.log(findAllKeysWithGivenValue(simpleObject, 1))
+console.log(findAllKeysWithGivenValue(simpleObject, 1))
 
-
+// ======================== Question 4 ===================================
 
 function findValuesOfGivenKey(obj, target){
     let arr = [];
