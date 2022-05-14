86b0ae8309a0ae4f04fe5c06a6f71fdb16f7b191
Patrick <papybp@gmail.com>
2022-05-14 02:08:17 -0500

    vgjhk

--- CS445/project/music-client/scripts/music.js
+++ CS445/project/music-client/scripts/music.js
@@ -43,6 +43,27 @@ window.onload = function () {
     document.getElementById("logout").onclick = logout;
     document.getElementsByClassName("searchBtn")[0].onclick = searchsong;
 
+    //   element.onclick = function(){
+    //     let songid = this.getAttribute("data-music");
+    //     console.log(songid);
+    //     fetch('http://localhost:3000/api/play/add',{
+    //         method: 'POST',
+    //         body:JSON.stringify({
+    //             songId: songid
+    //         }),
+    //         headers:{
+    //             'Authorization': `Bearer ${sessionStorage.getItem('tokenLogin')}`
+    //         }
+    //     }).then(res => res.json())
+    //     .then(data => {
+    //         console.log(data);
+    //     })
+    //   }
+    
+       
+       
+    
+     
 
 }
 function invalidLogin(msg) {
@@ -60,9 +81,6 @@ function loggedin() {
     document.getElementsByClassName("welcome")[0].style.display = "none";
     document.getElementsByClassName("tableContent")[0].style.display = "block";
     document.getElementById("playlist").style.display = "inline-table";
-
-
-
 
 }
 function logout() {
@@ -84,22 +102,25 @@ function fetchMusic() {
     }).then(res => res.json())
         .then(data => {
             let id = 1;
-            console.log(data);
             let tbody = document.getElementById("tbody");
             tbody.innerHTML = " ";
             data.forEach(element => {
-                
                 let tr = `<tr>
                         <td>${id}</td>
                         <td>${element.title}</td>
                         <td>${element.releaseDate}</td>
-                        <td><span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                        <td>
+                            <div class="plusbtn" data-music="${element.id}" >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                     fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
                                     <path
                                         d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                                     <path
                                         d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-                                </svg></span></td>
+                                </svg>
+                            </div>
+                            
+                        </td>
 
                         </tr>`;
                 tbody.innerHTML += tr;
@@ -168,7 +189,6 @@ function searchsong(){
     }).then(res => res.json())
     .then(data => {
         let id = 1;
-            console.log(data);
             let tbody = document.getElementById("tbody");
             tbody.innerHTML = " ";
             data.forEach(element => {
@@ -191,4 +211,15 @@ function searchsong(){
             });
             searchinput.value = "";
     })
+}
+
+function addToPlaylist(){
+    
+}
+
+let divs = document.getElementsByClassName("plusbtn");  
+    console.log(divs);
+for (let i = 0; i < divs.length; i++) {
+    console.log(divs[i]); ;
+    
 }
