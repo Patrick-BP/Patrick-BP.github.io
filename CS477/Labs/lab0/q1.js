185713a42c4f4d882a47891a1f890afba8a84145
Patrick <papybp@gmail.com>
2022-07-18 19:01:45 -0500

    kjhlkj

---
+++ CS477/Labs/lab0/q1.js
@@ -0,0 +1,14 @@
+"use strict";
+/* eslint-disable */
+const result = (function(exports, module){
+    exports = module.exports;
+    exports.firstname = 'John';
+    module.exports.lastname = 'Smith';
+    exports = {
+        getFullName: function(){
+            console.log('John Smith')
+        }
+    }
+    return module.exports;
+}).apply(null, [null, {exports: {}}]);
+console.log(result);
