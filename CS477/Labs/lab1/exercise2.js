ad8966428b7941a52c24ff2c01969e0baa1a2402
Patrick <papybp@gmail.com>
2022-07-20 15:41:40 -0500

    lkjlkj

--- CS477/Labs/lab1/exercise2.js
+++ CS477/Labs/lab1/exercise2.js
@@ -11,12 +11,13 @@ Explain why do we want sometimes to use setImmediate instead of using setTimeout
       -  because if you move the two calls within an I/O cycle, the immediate callback is always executed first
 
 Explain the difference between process.nextTick and setImmediate?
-      - process.nextTick is executed everytime an queue in the cycle is visited 
+      - process.nextTick is executed everytime after every phase in the cycle is visited 
       and has a higher priority
       - setImmediate is executed once in every cycle
+      - setImmediate is provided by libuv but nextTick is provided but node
 
 Name 10 global modules/methods available in Node environment.
-    setTimeOut(), SetInterval(), setImmediate(), require, module, global, buffer, 
+    setTimeOut(), SetInterval(), setImmediate(), require, module, global, buffer, clearSetTimeOut, clearSetInterval
     
     
     */
--- CS477/Labs/lab1/exercise3.js
+++ CS477/Labs/lab1/exercise3.js
@@ -3,9 +3,9 @@
 
 // Fix the slow function to be asynchronous/non-blocking
 function slow(callback){ 
-    
-	for(let i=0; i<= 5e8; i++){}
-	if (Math.random() > 0.5) { 	
+    let random = Math.random();
+    console.log(random);
+	if (random > 0.5) { 	
 		return callback("Error",null) 
 	} 
 	return callback(null, {id:12345}) 
@@ -13,15 +13,23 @@ function slow(callback){
 
 function exec(fn){ 
 	// Complete the code here to implement chaining with callback
-    return {
-        done:function(func){
-            
-        },
-        fail: function(func){
-          
+    let obj = {};
+    fn(function(err, data){
+        obj.done = function(fn){
+            fn(data);
+            return obj;
         }
-    }
-    
+        obj.fail = function(fn){
+            fn(err)
+            return obj;
+
+        }
+        if(err == null){
+            obj.fail = function(fun){return obj};
+        }else 
+            obj.done = function(fun){return obj};
+    });
+return obj;
 }
 
 exec(slow).done(function(data){ console.log(data); })
