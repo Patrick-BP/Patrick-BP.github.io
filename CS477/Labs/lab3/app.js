48104e870515980119b1e743c1e549e274123291
Patrick <papybp@gmail.com>
2022-07-22 23:45:26 -0500

    jklkljkl

--- CS477/Labs/lab3/app.js
+++ CS477/Labs/lab3/app.js
@@ -1,6 +1,5 @@
 
 "use strict";
-const { fstat } = require('fs');
 /* eslint-disable */
 /*
 1. Create a http server which listens to 3000 port.
@@ -12,15 +11,40 @@ title input, a blog body textarea and a submit button in the form
 
 4. On the server side, grab user's inputs and save into a file.
 
-5. After saving successfully, display "Save Successfully" page to user with a link goes back to home page.
+5. After saving successfully, display "Save Successfully" 
+page to user with a link goes back to home page.
 */
 
 const http = require('http');
 const fs = require('fs');
+const path = require('path')
 
 http.createServer((req, res)=>{
-if(req.url === "/"){
+if(req.url === "/" && req.method === 'GET'){
     res.end(fs.readFileSync('./index.html'))
+}else if(req.url=='/submitted.html' && req.method == 'POST'){
+    const body = [];
+    req.on('data',chunk =>{
+        body.push(chunk);
+    })
+    req.on('end', ()=>{
+        const content = Buffer.concat(body).toString();
+        const str = content.split('&')
+       
+
+        fs.writeFile(path.join(__dirname,'files', 'myfile.txt'), `${str[0]} ${str[1]}`, (err)=>{
+            if(err){
+                res.end('try again')
+            }else{
+                res.end(`<html><body>save successfully <br> <a href="/">home</a></body></html>`)
+            }
+        })
+    })
+   
 }
+
+
+
+
 
 }).listen(3000, ()=>console.log("listining on 3000 ...."))
