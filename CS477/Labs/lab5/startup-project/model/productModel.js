3c568567a4d15234b6f6baee5760101abb5d4404
Patrick <papybp@gmail.com>
2022-07-28 15:18:09 -0500

    lab5

---
+++ CS477/Labs/lab5/startup-project/model/productModel.js
@@ -0,0 +1,17 @@
+"use strict";
+/* eslint-disable */
+
+experts.Product = class{
+
+    constructor(id, title, price, description){
+        this.id = id;
+        this.title = this.title;
+        this.price = this.price;
+        this.description = this.description;
+    }
+
+
+    
+
+
+}
