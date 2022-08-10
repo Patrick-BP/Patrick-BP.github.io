6a630ecdbe6966ff6f893b1cb624b01360f7ecab
Patrick <papybp@gmail.com>
2022-08-09 22:24:31 -0500

    bmn,nm.,

--- CS477/Labs/project/client/js/home.js
+++ CS477/Labs/project/client/js/home.js
@@ -39,7 +39,7 @@ async function fetchTwites() {
             <div class="post__body">
               <div class="post__header">
                 <div class="post__headerText">
-                  <h3>${twite.user.fullname}<span class="time">@${createdAt}</span></h3>
+                  <h3>${twite.user.fullname}<span class="time"> - ${createdAt}</span></h3>
                 </div>
                 <div class="post__headerDescription">
                   <p> ${twite.tweet}</p>
